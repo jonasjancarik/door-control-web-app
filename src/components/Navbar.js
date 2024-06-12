6a9ec84afe263ece19c72131e80d259d9df8c2fb
Jonáš Jančařík <jonas.jancarik@gmail.com>
2024-06-12 16:32:16 +0200

    add icon-based menu for small screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,8 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 import { useState, useEffect, useCallback } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+// import { FaHome, FaCog, FaSignOutAlt } from 'react-icons/fa';
+import { MdHome, MdSettings, MdLogout  } from "react-icons/md";
 
 const AppNavbar = ({ user }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -21,9 +23,13 @@ const AppNavbar = ({ user }) => {
     return (
         <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
             <Container fluid>
-                <Navbar.Brand className='mb-auto' as={Link} href="/">{process.env.NEXT_PUBLIC_WEB_APP_TITLE}</Navbar.Brand>
+                <Navbar.Brand className='mb-auto d-lg-block d-none' as={Link} href="/">
+                    {process.env.NEXT_PUBLIC_WEB_APP_TITLE}
+                </Navbar.Brand>
                 {isAuthenticated && user ? (
                     <>
+                        <div className='ms-auto d-none d-lg-flex'>
+
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className='ms-auto'>
@@ -34,6 +40,21 @@ const AppNavbar = ({ user }) => {
                                 </Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
+                        </div>
+                        <div className="d-lg-none d-flex justify-content-around w-75 mx-auto">
+                            <Nav.Link as={Link} href="/">
+                                {/* <FaHome size={24} color='fffdf4' /> */}
+                                <MdHome size={24} color='fffdf4' />
+                            </Nav.Link>
+                            <Nav.Link as={Link} href="/settings">
+                                {/* <FaCog size={24} color="fffdf4" /> */}
+                                <MdSettings size={24} color="fffdf4" />
+                            </Nav.Link>
+                            <Nav.Link onClick={handleLogout} style={{ cursor: 'pointer' }}>
+                                {/* <FaSignOutAlt size={24} color="fffdf4" /> */}
+                                <MdLogout size={24} color="fffdf4" />
+                            </Nav.Link>
+                        </div>
                     </>
                 ) : null}
             </Container>
