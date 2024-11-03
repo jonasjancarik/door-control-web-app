937001ae8c215f1511b1f426cf22028ee2921698
Jonáš Jančařík <jonas.jancarik@gmail.com>
2024-11-03 22:23:15 +0100

    Reapply "Add PWA support to handle app URLs"
    This reverts commit a3cd089f3b164ea2c385ab8964308109359dc5fb.

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,25 @@ import withPWA from 'next-pwa';
 const nextConfig = withPWA({
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
+    sw: '/src/pwa/sw.js',
+    register: true,
+    scope: '/',
+    runtimeCaching: [
+        {
+            urlPattern: ({ url }) => {
+                const appUrl = process.env.NEXT_PUBLIC_APP_URL;
+                return url.origin === appUrl || url.pathname.startsWith(appUrl);
+            },
+            handler: 'NetworkFirst',
+            options: {
+                cacheName: 'app-cache',
+                expiration: {
+                    maxEntries: 32,
+                    maxAgeSeconds: 24 * 60 * 60 // 24 hours
+                }
+            }
+        }
+    ]
 })({
     output: 'export',
     env: {
@@ -13,6 +32,7 @@ const nextConfig = withPWA({
         NEXT_PUBLIC_WEB_APP_SUBTITLE: process.env.NEXT_PUBLIC_WEB_APP_SUBTITLE,
         NEXT_PUBLIC_WEB_APP_PORT: process.env.NEXT_PUBLIC_WEB_APP_PORT,
         NEXT_SENDER_EMAIL: process.env.NEXT_SENDER_EMAIL,
+        NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     },
 });
 
